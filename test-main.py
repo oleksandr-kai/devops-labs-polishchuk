412dfc0a27fffee09369f5991466d8faae6807a2
Oleksandr <8012592@stud.kai.edu.ua>
2026-04-13 11:18:58 +0300

    add unit test

---
+++ test-main.py
@@ -0,0 +1,12 @@
+import unittest
+from validators import validate_ip
+
+class TestValidators(unittest.TestCase):
+    def test_ip_validation_success(self):
+        self.assertTrue(validate_ip("192.168.1.1"))
+
+    def test_ip_validation_failure(self):
+        self.assertFalse(validate_ip("999.999.999.999"))
+
+if __name__ == "__main__":
+    unittest.main()
