bd0b5d1c678817adf204538a621ae4080b85aac2
Oleksandr <8012592@stud.kai.edu.ua>
2026-04-14 10:22:08 +0300

    add error in unit test

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from validators import validate_ip
 
 class TestValidators(unittest.TestCase):
     def test_ip_validation_success(self):
-        self.assertTrue(validate_ip("192.168.1.1"))
+        self.assertFalse(validate_ip("192.168.1.1"))
 
     def test_ip_validation_failure(self):
         self.assertTrue(validate_ip("999.999.999.999"))
